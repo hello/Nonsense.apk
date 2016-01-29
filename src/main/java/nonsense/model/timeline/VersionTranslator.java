ea1d3858f316794636fe5da28ede2b885fb9f5e4
km <km@sayhello.com>
2016-01-29 11:17:00 -0800

    add translator for moving between timeline v1 and v2

---
+++ src/main/java/nonsense/model/timeline/VersionTranslator.java
@@ -0,0 +1,189 @@
+package nonsense.model.timeline;
+
+import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.OptionalLong;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import nonsense.model.Condition;
+import nonsense.model.timeline.v1.TimelineV1;
+import nonsense.model.timeline.v1.TimelineV1Insight;
+import nonsense.model.timeline.v1.TimelineV1Segment;
+import nonsense.model.timeline.v1.TimelineV1Statistics;
+import nonsense.model.timeline.v2.ScoreCondition;
+import nonsense.model.timeline.v2.TimelineV2;
+import nonsense.model.timeline.v2.TimelineV2Event;
+import nonsense.model.timeline.v2.TimelineV2Metric;
+
+public class VersionTranslator {
+    public static TimelineV2 translate(TimelineV1 timeline) {
+        final List<TimelineV2Event> segments = timeline.segments.stream()
+                                                                .map(VersionTranslator::translateSegmentToEvent)
+                                                                .collect(Collectors.toList());
+        final List<TimelineV2Metric> metrics = translateTimelineInsightsToMetrics(timeline);
+
+        return new TimelineV2(OptionalInt.of(timeline.score),
+                              ScoreCondition.fromScore(timeline.score),
+                              timeline.message,
+                              timeline.date,
+                              segments,
+                              metrics);
+    }
+
+
+    //region Translating Metrics
+
+    public static List<TimelineV2Metric> translateStatisticsToMetrics(Optional<TimelineV1Statistics> maybeStatistics) {
+        final List<TimelineV2Metric> metrics = new ArrayList<>();
+        maybeStatistics.ifPresent(statistics -> {
+            metrics.add(new TimelineV2Metric(TimelineV2Metric.Name.TOTAL_SLEEP, statistics.totalSleep,
+                                             TimelineV2Metric.Unit.MINUTES, Condition.IDEAL));
+            metrics.add(new TimelineV2Metric(TimelineV2Metric.Name.SOUND_SLEEP, statistics.soundSleep,
+                                             TimelineV2Metric.Unit.MINUTES, Condition.IDEAL));
+            metrics.add(new TimelineV2Metric(TimelineV2Metric.Name.TIME_TO_SLEEP, statistics.timeToSleep,
+                                             TimelineV2Metric.Unit.MINUTES, Condition.IDEAL));
+            metrics.add(new TimelineV2Metric(TimelineV2Metric.Name.TIMES_AWAKE, statistics.timesAwake,
+                                             TimelineV2Metric.Unit.QUANTITY, Condition.IDEAL));
+
+            metrics.removeIf(metric -> !metric.value.isPresent());
+        });
+        return metrics;
+    }
+
+    public static List<TimelineV2Metric> translateSegmentsToMetrics(List<TimelineV1Segment> segments) {
+        final List<TimelineV2Metric> metrics = new ArrayList<>();
+
+        final OptionalLong fellAsleep = segments.stream()
+                                                .filter(segment -> segment.eventType == TimelineV1Segment.Type.SLEEP)
+                                                .mapToLong(segment -> segment.timestamp.toEpochSecond(ZoneOffset.UTC))
+                                                .findFirst();
+        fellAsleep.ifPresent(timestamp -> {
+            metrics.add(new TimelineV2Metric(TimelineV2Metric.Name.FELL_ASLEEP, OptionalLong.of(timestamp),
+                                             TimelineV2Metric.Unit.TIMESTAMP, Condition.IDEAL));
+        });
+
+        final OptionalLong wokeUp = segments.stream()
+                                            .filter(segment -> segment.eventType == TimelineV1Segment.Type.WAKE_UP)
+                                            .mapToLong(segment -> segment.timestamp.toEpochSecond(ZoneOffset.UTC))
+                                            .findFirst();
+        wokeUp.ifPresent(timestamp -> {
+            metrics.add(new TimelineV2Metric(TimelineV2Metric.Name.WOKE_UP, OptionalLong.of(timestamp),
+                                             TimelineV2Metric.Unit.TIMESTAMP, Condition.IDEAL));
+        });
+
+        return metrics;
+    }
+
+    public static List<TimelineV2Metric> translateInsightsToMetrics(List<TimelineV1Insight> insights) {
+        return insights.stream()
+                       .map(insight -> {
+                           final TimelineV2Metric.Name name = translateSensorToName(insight.sensor);
+                           return new TimelineV2Metric(name, OptionalLong.empty(),
+                                                       TimelineV2Metric.Unit.CONDITION,
+                                                       insight.condition);
+                       })
+                       .collect(Collectors.toList());
+    }
+
+    public static List<TimelineV2Metric> translateTimelineInsightsToMetrics(TimelineV1 timelineV1) {
+        final List<TimelineV2Metric> statisticMetrics = translateStatisticsToMetrics(timelineV1.statistics);
+        final List<TimelineV2Metric> segmentMetrics = translateSegmentsToMetrics(timelineV1.segments);
+        final List<TimelineV2Metric> insightMetrics = translateInsightsToMetrics(timelineV1.insights);
+
+        final List<TimelineV2Metric> metrics = new ArrayList<>(statisticMetrics.size() + segmentMetrics.size() + insightMetrics.size());
+        metrics.addAll(statisticMetrics);
+        metrics.addAll(segmentMetrics);
+        metrics.addAll(insightMetrics);
+        return metrics;
+    }
+
+    //endregion
+
+
+    //region Translating Events
+
+    public static TimelineV2Event translateSegmentToEvent(TimelineV1Segment segment) {
+        final TimelineV2Event.SleepState sleepState;
+        final TimelineV2Event.Type type;
+        if (segment.eventType == TimelineV1Segment.Type.NONE) {
+            sleepState = TimelineV2Event.SleepState.AWAKE;
+            type = TimelineV2Event.Type.IN_BED;
+        } else {
+            sleepState = TimelineV2Event.SleepState.fromSleepDepth(segment.sleepDepth);
+            type = translateEventType(segment.eventType);
+        }
+        return new TimelineV2Event(segment.timestamp,
+                                   segment.tzOffsetMillis,
+                                   TimeUnit.SECONDS.toMillis(segment.duration),
+                                   segment.message,
+                                   segment.sleepDepth,
+                                   sleepState,
+                                   type,
+                                   TimelineV2Event.Action.forEventType(type));
+    }
+
+    //endregion
+
+
+    //region Translating Enums
+
+    public static TimelineV2Metric.Name translateSensorToName(TimelineV1Insight.Sensor sensor) {
+        switch (sensor) {
+            case HUMIDITY:
+                return TimelineV2Metric.Name.HUMIDITY;
+            case LIGHT:
+                return TimelineV2Metric.Name.LIGHT;
+            case PARTICULATES:
+                return TimelineV2Metric.Name.PARTICULATES;
+            case SOUND:
+                return TimelineV2Metric.Name.SOUND;
+            case TEMPERATURE:
+                return TimelineV2Metric.Name.TEMPERATURE;
+            default:
+                return TimelineV2Metric.Name.UNKNOWN;
+        }
+    }
+
+    public static TimelineV2Event.Type translateEventType(TimelineV1Segment.Type type) {
+        switch (type) {
+            case ALARM:
+                return TimelineV2Event.Type.ALARM_RANG;
+            case IN_BED:
+                return TimelineV2Event.Type.GOT_IN_BED;
+            case LIGHT:
+                return TimelineV2Event.Type.LIGHT;
+            case LIGHTS_OUT:
+                return TimelineV2Event.Type.LIGHTS_OUT;
+            case MOTION:
+                return TimelineV2Event.Type.GENERIC_MOTION;
+            case NOISE:
+                return TimelineV2Event.Type.GENERIC_SOUND;
+            case OUT_OF_BED:
+                return TimelineV2Event.Type.GOT_OUT_OF_BED;
+            case PARTNER_MOTION:
+                return TimelineV2Event.Type.PARTNER_MOTION;
+            case SLEEP:
+                return TimelineV2Event.Type.FELL_ASLEEP;
+            case SUNRISE:
+                return TimelineV2Event.Type.SUNRISE;
+            case SUNSET:
+                return TimelineV2Event.Type.SUNSET;
+            case WAKE_UP:
+                return TimelineV2Event.Type.WOKE_UP;
+            case SLEEPING:
+                return TimelineV2Event.Type.IN_BED;
+            case SNORING:
+                return TimelineV2Event.Type.SNORED;
+            case SLEEP_TALK:
+                return TimelineV2Event.Type.SLEEP_TALKED;
+            default:
+                return TimelineV2Event.Type.UNKNOWN;
+        }
+    }
+
+    //endregion
+}
---
+++ src/test/java/nonsense/model/timeline/VersionTranslatorTests.java
@@ -0,0 +1,198 @@
+package nonsense.model.timeline;
+
+import com.google.common.collect.Lists;
+
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.OptionalLong;
+import java.util.stream.Collectors;
+
+import nonsense.model.Condition;
+import nonsense.model.timeline.v1.TimelineV1Insight;
+import nonsense.model.timeline.v1.TimelineV1Segment;
+import nonsense.model.timeline.v1.TimelineV1Statistics;
+import nonsense.model.timeline.v2.TimelineV2Event;
+import nonsense.model.timeline.v2.TimelineV2Metric;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
+
+public class VersionTranslatorTests {
+    private static final int TZ_OFFSET = ZoneOffset.UTC.getTotalSeconds() * 1000;
+
+    @Test
+    public void translateStatisticsToMetrics() {
+        final TimelineV1Statistics completeStatistics = new TimelineV1Statistics(OptionalLong.of(8),
+                                                                                 OptionalLong.of(1),
+                                                                                 OptionalLong.of(2),
+                                                                                 OptionalLong.of(9));
+        final List<TimelineV2Metric> completeMetrics = VersionTranslator.translateStatisticsToMetrics(Optional.of(completeStatistics));
+        assertThat(completeMetrics.size(), is(equalTo(4)));
+
+        final List<TimelineV2Metric.Name> completeMetricNames = completeMetrics.stream()
+                                                                               .map(m -> m.name)
+                                                                               .collect(Collectors.toList());
+        assertThat(completeMetricNames, hasItems(TimelineV2Metric.Name.SOUND_SLEEP,
+                                                 TimelineV2Metric.Name.TOTAL_SLEEP,
+                                                 TimelineV2Metric.Name.TIME_TO_SLEEP,
+                                                 TimelineV2Metric.Name.TIMES_AWAKE));
+
+        final TimelineV1Statistics incompleteStatistics = new TimelineV1Statistics(OptionalLong.of(8),
+                                                                                   OptionalLong.empty(),
+                                                                                   OptionalLong.of(2),
+                                                                                   OptionalLong.empty());
+        final List<TimelineV2Metric> incompleteMetrics = VersionTranslator.translateStatisticsToMetrics(Optional.of(incompleteStatistics));
+        assertThat(incompleteMetrics.size(), is(equalTo(2)));
+
+        final List<TimelineV2Metric.Name> incompleteMetricNames = incompleteMetrics.stream()
+                                                                                   .map(m -> m.name)
+                                                                                   .collect(Collectors.toList());
+        assertThat(incompleteMetricNames, hasItems(TimelineV2Metric.Name.SOUND_SLEEP,
+                                                   TimelineV2Metric.Name.TIMES_AWAKE));
+
+
+        final List<TimelineV2Metric> noMetrics = VersionTranslator.translateStatisticsToMetrics(Optional.empty());
+        assertThat(noMetrics.size(), is(equalTo(0)));
+    }
+
+    @Test
+    public void translateSegmentsToMetrics() {
+        final LocalDateTime now = LocalDateTime.now();
+        final TimelineV1Segment fellAsleep = new TimelineV1Segment(100, TimelineV1Segment.Type.SLEEP,
+                                                                   0, Optional.empty(),
+                                                                   TZ_OFFSET, 50,
+                                                                   now.minusHours(10L),
+                                                                   Collections.emptyList());
+        final TimelineV1Segment wokeUp = new TimelineV1Segment(100, TimelineV1Segment.Type.WAKE_UP,
+                                                               0, Optional.empty(),
+                                                               TZ_OFFSET, 50,
+                                                               now.minusHours(10L),
+                                                               Collections.emptyList());
+
+        final List<TimelineV2Metric> metrics = VersionTranslator.translateSegmentsToMetrics(Lists.newArrayList(fellAsleep, wokeUp));
+        assertThat(metrics.size(), is(equalTo(2)));
+
+        final TimelineV2Metric fellAsleepMetric = metrics.get(0);
+        assertThat(fellAsleepMetric.name, is(equalTo(TimelineV2Metric.Name.FELL_ASLEEP)));
+        assertThat(fellAsleepMetric.unit, is(equalTo(TimelineV2Metric.Unit.TIMESTAMP)));
+        assertThat(fellAsleepMetric.condition, is(equalTo(Condition.IDEAL)));
+        assertThat(fellAsleepMetric.value, is(not(equalTo(OptionalLong.empty()))));
+
+        final TimelineV2Metric wokeUpMetric = metrics.get(1);
+        assertThat(wokeUpMetric.name, is(equalTo(TimelineV2Metric.Name.WOKE_UP)));
+        assertThat(wokeUpMetric.unit, is(equalTo(TimelineV2Metric.Unit.TIMESTAMP)));
+        assertThat(wokeUpMetric.condition, is(equalTo(Condition.IDEAL)));
+        assertThat(wokeUpMetric.value, is(not(equalTo(OptionalLong.empty()))));
+    }
+
+    @Test
+    public void translateInsightsToMetrics() {
+        final List<TimelineV1Insight> insights = new ArrayList<>();
+        insights.add(new TimelineV1Insight(Condition.ALERT,
+                                           "Consider moving somewhere better",
+                                           TimelineV1Insight.Sensor.PARTICULATES));
+        insights.add(new TimelineV1Insight(Condition.WARNING,
+                                           "It's awfully warm in here...",
+                                           TimelineV1Insight.Sensor.TEMPERATURE));
+
+        final List<TimelineV2Metric> metrics = VersionTranslator.translateInsightsToMetrics(insights);
+        assertThat(metrics.size(), is(equalTo(2)));
+
+        final TimelineV2Metric particulates = metrics.get(0);
+        assertThat(particulates.name, is(equalTo(TimelineV2Metric.Name.PARTICULATES)));
+        assertThat(particulates.condition, is(equalTo(Condition.ALERT)));
+        assertThat(particulates.unit, is(equalTo(TimelineV2Metric.Unit.CONDITION)));
+        assertThat(particulates.value, is(OptionalLong.empty()));
+
+        final TimelineV2Metric temperature = metrics.get(1);
+        assertThat(temperature.name, is(equalTo(TimelineV2Metric.Name.TEMPERATURE)));
+        assertThat(temperature.condition, is(equalTo(Condition.WARNING)));
+        assertThat(temperature.unit, is(equalTo(TimelineV2Metric.Unit.CONDITION)));
+        assertThat(temperature.value, is(OptionalLong.empty()));
+    }
+
+    @Test
+    public void translateSegmentToEvent() {
+        final TimelineV1Segment awakeInBed = new TimelineV1Segment(100, TimelineV1Segment.Type.NONE,
+                                                                   0, Optional.empty(),
+                                                                   TZ_OFFSET, 50,
+                                                                   LocalDateTime.now(),
+                                                                   Collections.emptyList());
+        final TimelineV2Event awakeInBedEvent = VersionTranslator.translateSegmentToEvent(awakeInBed);
+        assertThat(awakeInBedEvent.sleepState, is(equalTo(TimelineV2Event.SleepState.AWAKE)));
+        assertThat(awakeInBedEvent.type, is(equalTo(TimelineV2Event.Type.IN_BED)));
+
+        final TimelineV1Segment normalSegment = new TimelineV1Segment(60, TimelineV1Segment.Type.WAKE_UP,
+                                                                      0, Optional.of("You woke up"),
+                                                                      TZ_OFFSET, 50, LocalDateTime.now(),
+                                                                      Collections.emptyList());
+        final TimelineV2Event normalEvent = VersionTranslator.translateSegmentToEvent(normalSegment);
+        assertThat(normalEvent.type, is(equalTo(TimelineV2Event.Type.WOKE_UP)));
+        assertThat(normalEvent.sleepState, is(equalTo(TimelineV2Event.SleepState.MEDIUM)));
+        assertThat(normalEvent.sleepDepth, is(equalTo(50)));
+        assertThat(normalEvent.durationMillis, is(equalTo(60 * 1000L)));
+        assertThat(normalEvent.message, is(equalTo(Optional.of("You woke up"))));
+        assertThat(normalEvent.timestamp, is(equalTo(normalSegment.timestamp)));
+        assertThat(normalEvent.timezoneOffset, is(equalTo(TZ_OFFSET)));
+        assertThat(normalEvent.validActions, hasItems(TimelineV2Event.Action.ADJUST_TIME,
+                                                      TimelineV2Event.Action.VERIFY,
+                                                      TimelineV2Event.Action.INCORRECT));
+    }
+
+    @Test
+    public void translateSensorToName() {
+        assertThat(VersionTranslator.translateSensorToName(TimelineV1Insight.Sensor.HUMIDITY),
+                   is(equalTo(TimelineV2Metric.Name.HUMIDITY)));
+        assertThat(VersionTranslator.translateSensorToName(TimelineV1Insight.Sensor.LIGHT),
+                   is(equalTo(TimelineV2Metric.Name.LIGHT)));
+        assertThat(VersionTranslator.translateSensorToName(TimelineV1Insight.Sensor.PARTICULATES),
+                   is(equalTo(TimelineV2Metric.Name.PARTICULATES)));
+        assertThat(VersionTranslator.translateSensorToName(TimelineV1Insight.Sensor.SOUND),
+                   is(equalTo(TimelineV2Metric.Name.SOUND)));
+        assertThat(VersionTranslator.translateSensorToName(TimelineV1Insight.Sensor.TEMPERATURE),
+                   is(equalTo(TimelineV2Metric.Name.TEMPERATURE)));
+    }
+
+    @Test
+    public void translateEventType() {
+        assertThat(VersionTranslator.translateEventType(TimelineV1Segment.Type.ALARM),
+                   is(equalTo(TimelineV2Event.Type.ALARM_RANG)));
+        assertThat(VersionTranslator.translateEventType(TimelineV1Segment.Type.IN_BED),
+                   is(equalTo(TimelineV2Event.Type.GOT_IN_BED)));
+        assertThat(VersionTranslator.translateEventType(TimelineV1Segment.Type.LIGHT),
+                   is(equalTo(TimelineV2Event.Type.LIGHT)));
+        assertThat(VersionTranslator.translateEventType(TimelineV1Segment.Type.LIGHTS_OUT),
+                   is(equalTo(TimelineV2Event.Type.LIGHTS_OUT)));
+        assertThat(VersionTranslator.translateEventType(TimelineV1Segment.Type.MOTION),
+                   is(equalTo(TimelineV2Event.Type.GENERIC_MOTION)));
+        assertThat(VersionTranslator.translateEventType(TimelineV1Segment.Type.NOISE),
+                   is(equalTo(TimelineV2Event.Type.GENERIC_SOUND)));
+        assertThat(VersionTranslator.translateEventType(TimelineV1Segment.Type.OUT_OF_BED),
+                   is(equalTo(TimelineV2Event.Type.GOT_OUT_OF_BED)));
+        assertThat(VersionTranslator.translateEventType(TimelineV1Segment.Type.PARTNER_MOTION),
+                   is(equalTo(TimelineV2Event.Type.PARTNER_MOTION)));
+        assertThat(VersionTranslator.translateEventType(TimelineV1Segment.Type.SLEEP),
+                   is(equalTo(TimelineV2Event.Type.FELL_ASLEEP)));
+        assertThat(VersionTranslator.translateEventType(TimelineV1Segment.Type.SUNRISE),
+                   is(equalTo(TimelineV2Event.Type.SUNRISE)));
+        assertThat(VersionTranslator.translateEventType(TimelineV1Segment.Type.SUNSET),
+                   is(equalTo(TimelineV2Event.Type.SUNSET)));
+        assertThat(VersionTranslator.translateEventType(TimelineV1Segment.Type.WAKE_UP),
+                   is(equalTo(TimelineV2Event.Type.WOKE_UP)));
+        assertThat(VersionTranslator.translateEventType(TimelineV1Segment.Type.SLEEPING),
+                   is(equalTo(TimelineV2Event.Type.IN_BED)));
+        assertThat(VersionTranslator.translateEventType(TimelineV1Segment.Type.SNORING),
+                   is(equalTo(TimelineV2Event.Type.SNORED)));
+        assertThat(VersionTranslator.translateEventType(TimelineV1Segment.Type.SLEEP_TALK),
+                   is(equalTo(TimelineV2Event.Type.SLEEP_TALKED)));
+    }
+}
