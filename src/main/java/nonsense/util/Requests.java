c58c0d8518bc137a2efaa09d3349400ecd2694ab
km <km@sayhello.com>
2016-01-29 16:01:58 -0800

    add utility class for more easily extracting query params

---
+++ src/main/java/nonsense/util/Requests.java
@@ -0,0 +1,56 @@
+package nonsense.util;
+
+import java.time.LocalDate;
+import java.time.ZoneOffset;
+import java.util.Locale;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import spark.Request;
+
+public class Requests {
+    public static final String TIME_ZONE = "ns_time_zone";
+    public static final String LOCALE = "ns_locale";
+    public static final String TODAY = "ns_today";
+    public static final String ACCOUNT_AGE = "ns_account_age";
+
+    private static <T> T queryParamValue(Request request,
+                                         String name,
+                                         Function<String, T> factory,
+                                         Supplier<T> getDefault) {
+        if (request.queryParams().contains(name)) {
+            final String param = request.queryParams(name);
+            return factory.apply(param);
+        } else {
+            return getDefault.get();
+        }
+    }
+
+    public static int queryParamInteger(Request request, String name, int defaultValue) {
+        return queryParamValue(request, name,
+                               Integer::parseInt,
+                               () -> defaultValue);
+    }
+
+    public static Locale queryParamLocale(Request request, String name) {
+        return queryParamValue(request, name,
+                               Locale::forLanguageTag,
+                               Locale::getDefault);
+    }
+
+    public static LocalDate queryParamLocalDate(Request request, String name, Supplier<LocalDate> getDefault) {
+        return queryParamValue(request, name,
+                               LocalDate::parse,
+                               getDefault);
+    }
+
+    public static LocalDate queryParamLocalDate(Request request, String name) {
+        return queryParamLocalDate(request, name, LocalDate::now);
+    }
+
+    public static ZoneOffset queryParamTimeZone(Request request, String name) {
+        return queryParamValue(request, name,
+                               ZoneOffset::of,
+                               () -> ZoneOffset.UTC);
+    }
+}
---
+++ src/test/java/nonsense/util/RequestsTests.java
@@ -0,0 +1,90 @@
+package nonsense.util;
+
+import com.google.common.collect.Sets;
+
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.ZoneOffset;
+import java.util.Collections;
+import java.util.Locale;
+
+import spark.Request;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+
+public class RequestsTests {
+    private static Request emptyRequest() {
+        final Request request = mock(Request.class);
+        doReturn(Collections.emptySet())
+                .when(request)
+                .queryParams();
+        doReturn(null)
+                .when(request)
+                .queryParams(any());
+        return request;
+    }
+
+    private static Request requestWithQueryParam(String name, String value) {
+        final Request request = mock(Request.class);
+        doReturn(Sets.newHashSet(name))
+                .when(request)
+                .queryParams();
+        doReturn(value)
+                .when(request)
+                .queryParams(name);
+        return request;
+    }
+
+    @Test
+    public void queryParamInteger() {
+        final Request withAccountAge = requestWithQueryParam(Requests.ACCOUNT_AGE, "80");
+        assertThat(Requests.queryParamInteger(withAccountAge, Requests.ACCOUNT_AGE, 90),
+                   is(equalTo(80)));
+
+        final Request withoutAccountAge = emptyRequest();
+        assertThat(Requests.queryParamInteger(withoutAccountAge, Requests.ACCOUNT_AGE, 90),
+                   is(equalTo(90)));
+    }
+
+    @Test
+    public void queryParamLocale() {
+        final Request withLocale = requestWithQueryParam(Requests.LOCALE, "en_UK");
+        assertThat(Requests.queryParamLocale(withLocale, Requests.LOCALE),
+                   is(equalTo(Locale.forLanguageTag("en_UK"))));
+
+        final Request withoutLocale = emptyRequest();
+        assertThat(Requests.queryParamLocale(withoutLocale, Requests.LOCALE),
+                   is(equalTo(Locale.US)));
+    }
+
+    @Test
+    public void queryParamLocalDate() {
+        final LocalDate now = LocalDate.now();
+
+        final Request withLocale = requestWithQueryParam(Requests.LOCALE, "2015-12-21");
+        assertThat(Requests.queryParamLocalDate(withLocale, Requests.LOCALE, () -> now),
+                   is(equalTo(LocalDate.of(2015, 12, 21))));
+
+        final Request withoutLocale = emptyRequest();
+        assertThat(Requests.queryParamLocalDate(withoutLocale, Requests.LOCALE, () -> now),
+                   is(equalTo(now)));
+    }
+
+    @Test
+    public void queryParamTimeZone() {
+        final Request withTimeZone = requestWithQueryParam(Requests.TIME_ZONE, "-8");
+        assertThat(Requests.queryParamTimeZone(withTimeZone, Requests.TIME_ZONE),
+                   is(equalTo(ZoneOffset.of("-8"))));
+
+        final Request withoutTimeZone = emptyRequest();
+        assertThat(Requests.queryParamTimeZone(withoutTimeZone, Requests.TIME_ZONE),
+                   is(equalTo(ZoneOffset.UTC)));
+    }
+}
